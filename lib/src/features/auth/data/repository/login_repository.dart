3f9f96b3a79ce8fc9b97d42e4bb40d8388415984
Matheus Henrique <48736650+Matheus872@users.noreply.github.com>
2022-06-15 23:21:09 -0300

    clear master branch

--- lib/src/features/auth/data/repository/login_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../../domain/model/user.dart';
-import '../../domain/repository/login_interface.dart';
-
-class LoginRepository implements ILogin {
-  @override
-  Future<User> login(User user) {
-    throw UnimplementedError();
-  }
-}
